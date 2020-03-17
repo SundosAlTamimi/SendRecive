53a52461089b41830d1526aa065c117e003f3a0b
sundos <sundos.tamimi20@gmail.com>
2020-03-17 13:21:51 +0200

    Initial commit

---
+++ app/src/main/java/com/example/sendrecive/PrintDSD.java
@@ -0,0 +1,14 @@
+package com.example.sendrecive;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PrintDSD extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_print_dsd);
+    }
+}
